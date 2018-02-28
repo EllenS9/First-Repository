80e888dcc0c4c96f70906b277aea040412746d9c
EllenS9 <36919601+EllenS9@users.noreply.github.com>
2018-02-28 21:50:19 +0000

    Add files via upload

---
+++ Inequalities Python & BMI calculator no2.py
@@ -0,0 +1,84 @@
+
+# coding: utf-8
+
+# In[4]:
+
+
+a= 1
+b=2
+if a < b:
+    print ("a is less than b")
+    print ("a is definitely less than b")
+print ("not sure if a is less than b")
+
+
+# In[6]:
+
+
+c=5
+d=4
+if c<d:
+    print ("c is less than d")
+else:
+    print ("c is not less than d")
+    print ("i don't think c is less than d")
+print ("outside the if block")
+
+
+# In[12]:
+
+
+e=20
+f=8
+if e<f:
+    print ("e is less than f")
+elif e==f:
+    print ("e is equal to f")
+elif e> f+ 10:
+    print ("e is greater than f by more than 10")
+else:
+    print ("e is greater than f")
+
+
+# In[18]:
+
+
+g=90
+h=8
+if g< h:
+    print ("g is less than h")
+else:
+    if g == h:
+        print ("g is equal to h")
+    else :
+        print("g is greater than h")
+    
+
+
+# In[55]:
+
+
+name="Ellie"
+gender = "f"
+height_m = 1.40
+weight_kg = 50
+
+if(gender == "f"):
+    his_or_her = "her"
+elif(gender == "m"):
+    his_or_her = "his"
+else:
+    his_or_her = name + "'s"
+        
+bmi= weight_kg/ (height_m ** 2)
+print (bmi)
+if bmi < 18:
+    print (name + " is underweight. " + name +  "is suggested to increase" + his_or_her + " calorie intake & to get plenty of exercise.")
+else:
+    if 18< bmi <25:
+        print (name + " is normal weight. It is suggested that " + name + "  keep " + his_or_her + "calorie intake the same & maintain activity levels")
+    if 25< bmi <30:
+        print (name + " is overweight. It is suggested that " + name + " reviewed " + his_or_her + " calorie intake & activity levels")
+    if bmi >30: 
+        print (name + " is obese. It is suggested that " + name + " went to see a GP because there may be at risk of type 2 diabetes & heart disease.")
+
